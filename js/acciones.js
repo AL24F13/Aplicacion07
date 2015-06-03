ab4a55dfa227a1d05a467161539ed6bfc49cbe98
AL24F13 <mortallrulax27@gmail.com>
2015-06-03 14:49:29 -0500

    juego
    Aplicacion07

---
+++ js/acciones.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX ('B1','audio/C.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B2','audio/D.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B3','audio/E.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX ('B4','audio/F.mp3',function() {},
+	function(msg){alert("error "+msg);});
+	
+$('#btnjugar').on ('tap',function(){
+
+	var pantalla=$.mobile.getScreenHeight();
+	var encabezado=$('.ui-header').outerHeight();
+	var pie=$('.ui-footer').outerHeight();
+	var contenido=$('.ui-content').outerHeight();
+	//alert ('pantalla ' + pantalla);
+	//alert ('encabezado ' + encabezado);
+	//alert ('pie' + pie);
+	//alert ('contenido ' + contenido);
+	var alto=(pantalla-encabezado-pie)/2;
+	$('.cuadro').height(alto);
+	
+	
+	
+});//btnjugar
+
+
+$('.cuadro').on ('vmousedown', function(){
+	
+	$('#pantalla').append (quien ($ (this).attr('id')));
+	$(this).addClass('pulsado');
+	});	
+	
+	$('.cuadro').on ('vmouseup', function(){
+	$(this).removeClass('pulsado');	
+    });	
+	
+});//cuadro
+
+function quien (q)
+{
+ audio.play(q);
+ return q.substring(1);
+}
+
+});
+
