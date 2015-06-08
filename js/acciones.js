a8d2e7bc2743ff60441b6127d0ad113eb450705a
AL24F13 <mortallrulax27@gmail.com>
2015-06-08 08:19:01 -0500

    DOS
    proyectocolores

--- js/acciones.js
+++ js/acciones.js
@@ -1,51 +1,68 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	var basedatos=window.sqlitePlugin.openDatabase({name:"coloresBD.db",createFromLocation:1});
+		function cargarnombrejugador ()
+		{
+			basedatos.transaction(function(ejecutar){
+				var sql="SELECT NombreUsuario FROM Usuario";
+				
+				
+				ejecutar.executesql(sql,undefined,function(ejecutar,resultado){
+					var datosjugador=resultado.row.item(0);
+					$('#jugador').text(datosjugador.nombreusuario)
+					
+					
+					
+			});
+			});
+					 
+				
+				
+		}
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX ('B1','audio/C.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B2','audio/D.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B3','audio/E.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	audio.preloadFX ('B4','audio/F.mp3',function() {},
-	function(msg){alert("error "+msg);});
-	
-$('#btnjugar').on ('tap',function(){
-
-	var pantalla=$.mobile.getScreenHeight();
-	var encabezado=$('.ui-header').outerHeight();
-	var pie=$('.ui-footer').outerHeight();
-	var contenido=$('.ui-content').outerHeight();
-	//alert ('pantalla ' + pantalla);
-	//alert ('encabezado ' + encabezado);
-	//alert ('pie' + pie);
-	//alert ('contenido ' + contenido);
-	var alto=(pantalla-encabezado-pie)/2;
-	$('.cuadro').height(alto);
+	audio.preloadFX('B1','audio/C.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B2','audio/D.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B3','audio/E.pm3',function(){},function(msg){ alert ("error" + msg);});
+	audio.preloadFX('B4','audio/F.pm3',function(){},function(msg){ alert ("error" + msg);});
 	
 	
-	
-});//btnjugar
-
-
-$('.cuadro').on ('vmousedown', function(){
-	
-	$('#pantalla').append (quien ($ (this).attr('id')));
-	$(this).addClass('pulsado');
-	});	
-	
-	$('.cuadro').on ('vmouseup', function(){
-	$(this).removeClass('pulsado');	
-    });	
-	
-});//cuadro
-
-function quien (q)
-{
- audio.play(q);
- return q.substring(1);
-}
-
+	$('#btnjugar').on('tap',function(){
+		var pantalla =$.mobile.getScreenHeight();
+		var encabezado =$('.ui-header').outerHeight();
+		var pie =$('.ui-footer').outerHeight();
+		var contenido =$('.ui-content').outerHeight();
+		var alto=(pantalla - encabezado - pie )/2;
+		//alert ('pantalla ' + pantalla);
+		//alert ('encabezado ' + encabezado);
+		//alert ('pie' + pie);
+		//alert ('contenido' + contenido);
+		
+		$('.cuadro').height(alto);
+		
+		}); //cuadro
+		
+		
+		$ ('.cuadro').on('vmousedown', function (){
+			$('#pantalla').append(quien($(this).attr('id')));
+		$(this).addClass('pulsado');
+			}); //btnjugar
+			
+			$ ('.cuadro').on('vmouseup', function (){
+			$(this).removeClass('pulsado');
+			}); //btnjugar
+			
+			function quien (q)
+			{
+				audio.play(q);
+				return q.substring (1);
+				
+			}
+			
+		
+			
+			
+			
+}); 
 });
 
